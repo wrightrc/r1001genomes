f7c2c75698e5a8faf0258d23e1eb41f5faaac5e5
Clay Wright <wright.clay@gmail.com>
2017-12-27 18:47:44 -0800

    Test for subtree, updated label_bySNPs documentation

--- R/VCF_Utils.R
+++ R/VCF_Utils.R
@@ -541,12 +541,14 @@ add_ecotype_details <- function(data, Ecotype_column="Indiv") {
 #   return(indivGT)
 # }
 
-#' Title
+#' Label
 #'
-#' @param data
-#' @param collapse
+#' @param data a data frame of variants with a column named "Indiv"
+#' @param collapse should each accession will be a single line
 #'
-#' @return
+#' @return a df with a single row per accession, with a new column "SNPs" that
+#'  has a single text string detailing all the variants from data within that
+#'  accession
 #' @export
 #'
 #' @examples
