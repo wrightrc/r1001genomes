daa4a60f12eac385c72bd90ab062e67355064138
Morgan Hamm <morganohamm@gmail.com>
2018-02-09 17:26:55 -0800

    updated plotCodingDiv to color by a new "class" field derived from the Effect. Coloring is now consistent across genes with differing effects present.

--- R/VCF_Utils.R
+++ R/VCF_Utils.R
@@ -554,33 +554,26 @@ getCodingDiv <- function(data){
 #'
 #' @examples
 plotCodingDiv <- function(uniqueCodingVars){
-  # build a color data.frame
-  effectColor <- data.frame("color" = brewer.pal(n = 8, name = "RdYlBu")[8:1],
-                            "Effect" = c("synonymous_variant",
-                                         "stop_retained_variant",
-                      "5_prime_UTR_premature_start_codon_gain_variant",
-                                         "splice_donor_variant",
-                      "splice_region_variant",
-                                         "missense_variant",
-                      "frameshift_variant",
-                                         "stop_gained"),
-                            "labels" = c("synonymous", "stop_retained",
-                                         "premature start codon",
-                                         "splice donor", "splice region",
-                                         "missense", "frameshift",
-                                         "stop gained"))
-  # join the color data.frame with coding_vcf
-  uniqueCodingVars <- uniqueCodingVars %>%
-    dplyr::left_join(effectColor, by = "Effect")
-  effects <- effectColor$Effect %in% unique(uniqueCodingVars$Effect)
+
+  effectClasses <- readRDS(system.file("data", "effect_classes.rds", package="r1001genomes"))
+
+  classColors <- data.frame("color" = brewer.pal(n = 5, name = "RdYlBu")[5:1],
+                            "Class" = c("Synonymous", "Non_Coding", "Splice",
+                                        "Missense", "Nonsense"),
+                            "labels" = c("Synonymous", "Non_Coding", "Splice",
+                                        "Missense", "Nonsense"), stringsAsFactors=FALSE)
+
+  uniqueCodingVars <- dplyr::left_join(uniqueCodingVars, effectClasses, by = "Effect")
+  uniqueCodingVars <- dplyr::left_join(uniqueCodingVars, classColors, by = "Class")
+  classes <- classColors$Class %in% unique(uniqueCodingVars$Class)
   #plot the diversity
   plot <- ggplot(uniqueCodingVars, aes(x=Codon_Number,y=Diversity, colour=color, shape = Effect)) +
     geom_point(size = 4) +
     scale_y_log10(breaks=c(0.0001, 0.001, 0.01, 0.1),limits=c(0.0001, 1)) +
     #scale_colour_manual(values=c(synonymous_diversity="blue", missense_diversity="red")) +
     ylab("nucleotide diversity, log scale") + theme_few(base_size = 18) +
-    scale_color_identity("Effect", breaks = effectColor$color[effects],
-                         labels = effectColor$labels[effects],
+    scale_color_identity("Class", breaks = classColors$color[classes],
+                         labels = classColors$labels[classes],
                          guide = "legend")
   return(plot)
 }
